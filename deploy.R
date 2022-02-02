ab86f7e7f90b4216b5a19034cf0f66c35fb0d786
Aileen McDonald <aileen_mcdonald14@hotmail.com>
2022-02-02 18:34:56 +0000

    preprocessing, app, cron instructions & renv

---
+++ deploy.R
@@ -0,0 +1,13 @@
+library(rsconnect)
+
+setAccountInfo(name = Sys.getenv("SHINY_ACC_NAME"),
+               token = Sys.getenv("TOKEN"),
+               secret = Sys.getenv("SECRET"))
+
+# Initial deployment setting name 
+deployApp(
+  appFiles = c("app.R", "data_output"),
+  appName = Sys.getenv("MAINNAME"),
+  account = Sys.getenv("SHINY_ACC_NAME"),
+  appTitle = "shinyapplication")
+
---
+++ processing_script.R
@@ -0,0 +1,28 @@
+# Pulls DEFRA Flood warning API and counts warnings grouped by severity & returns most recent change time
+
+# Load packages
+library(httr)
+library(jsonlite)
+library(tidyverse)
+library(janitor)
+library(lubridate)
+
+# Call DEFRA flood API
+query <- "http://environment.data.gov.uk/flood-monitoring/id/floods/"
+response <- GET(query)
+content <- content(response, type = "text", encoding = "UTF-8")
+
+# Clean JSON to dataframe
+floods <- content |>
+  fromJSON() |>
+  pluck("items") |>
+  clean_names()
+
+flood_counts <- floods |>
+  filter(severity != "Warning no longer in force") |>
+  group_by(severity_level) |>
+  summarise(count = n()) |>
+  mutate(time_code_run = Sys.time())
+  
+flood_counts |>
+  saveRDS("~/analysis_work/testing/techstacktest/data_output/flood_counts.rds")
